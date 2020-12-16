7dcc5afc21239dd66ca0b5ce13247659212b3672
Vaibhav Bhawalkar <vaibhavb98.vb@gmail.com>
2020-12-16 10:11:01 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import reveal from 'reveal.js';
+import highlight from 'highlight.js';
+// import './js/chart/stars-history';
+// import './js/chart/benchmark';
+// import './js/chart/memory';
+
+reveal.initialize({
+  history: true,
+  transition: 'slide',
+});
+
+highlight.initHighlightingOnLoad();
